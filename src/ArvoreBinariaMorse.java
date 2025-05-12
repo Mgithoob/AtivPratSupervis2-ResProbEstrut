411e0989983bb16fedcec86fdc5381bf25c10be3
Mgithoob <132958332+Mgithoob@users.noreply.github.com>
2025-05-11 23:29:18 -0300

    Update ArvoreBinariaMorse.java

--- src/ArvoreBinariaMorse.java
+++ src/ArvoreBinariaMorse.java
@@ -49,7 +49,7 @@ public class ArvoreBinariaMorse {
             {'-', '-', '-', '-', '.'}    // 9
     };
 
-    private Node raiz;
+    Node raiz;
 
     class Node {
         private char valor;
@@ -166,34 +166,7 @@ public class ArvoreBinariaMorse {
     }
 
     public static void main(String[] args) {
-        ArvoreBinariaMorse arvore = new ArvoreBinariaMorse();
 
-        System.out.println("Árvore a partir do construtor:\n\n");
-        arvore.desenhar(arvore.raiz, 0);
-
-        System.out.println("Inserindo os caracteres A-Z, 0-9...");
-        // Inserindo caracteres A-Z
-        for (char i = 65; i <= 90; i++) {
-            arvore.inserir(i);
-        }
-        // Inserindo caracteres 0-9
-        for (char i = 48; i <= 57; i++) {
-            arvore.inserir(i);
-        }
-
-        System.out.println("Árvore Desenhada:\n\n");
-        arvore.desenhar(arvore.raiz, 0);
-
-        // As strings devem ter suas letras separadas por espaços.
-        String teste = "... --- ...";
-        String todo_morse= ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- " +
-                "-. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --.. " +
-                "----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----.";
-
-
-        System.out.println(arvore.buscar(teste));
-        System.out.println(arvore.buscar(todo_morse));
     }
 
 }
-
